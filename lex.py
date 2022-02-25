21e967343e723569e408514973d89804ff23032e
g-poulos <cs04480@uoi.gr>
2022-02-25 19:00:10 +0200

    Startup

---
+++ lex.py
@@ -0,0 +1,17 @@
+from json.tool import main
+
+
+class Token:
+    def __init__(self, recognized_string, family, line_number):
+        self.recognized_string = recognized_string
+        self.family = family
+        self.line_number = line_number
+
+
+
+
+def main(): 
+    print("zello")
+
+if __name__ == "__main__":
+    main()
