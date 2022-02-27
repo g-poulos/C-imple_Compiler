80310750ee2f3ac5957dc9f9e0da9f702bb99420
g-poulos <cs04480@uoi.gr>
2022-02-27 21:37:51 +0200

    clearBlankChar

--- lex.py
+++ lex.py
@@ -38,24 +38,38 @@ def isSimple(file):
         return character, "groupSymbol"
     elif(character in delimeter_list):
         return character, "delimeter"
+    else:
+        return "", ""
+
+def clearBlankChar(file):
+    character = file.read(1)
+    while(True):
+        print(character)
+        if(not character.isspace()):
+            file.seek(file.tell()-1)
+            break
+        character = file.read(1)
+
 
 def lex(file):
     family = ""
     line_counter = 0
 
-    #recognized_string, family = isNumber(file)
+    clearBlankChar(file)
+
+    recognized_string, family = isNumber(file)
     #recognized_string, family = isSimple(file)
-    recognized_string, family = isKeyword(file)
+    #recognized_string, family = isKeyword(file)
 
     print(recognized_string + " family: " + family + " line: ", line_counter)
     return Token(recognized_string, family, line_counter)
 
 def main():
-    if(len(sys.argv) < 3):
-            print(sys.argv[1]) 
+    #if(len(sys.argv) < 3):
+            #print(sys.argv[1]) 
 
-    file = open(sys.argv[1], "r")
-    lex(file)
+    with open(sys.argv[1], "r") as file:
+        lex(file)
 
 if __name__ == "__main__":
     main()
