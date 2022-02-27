54cbb48fa618a1d36b951c4ee956b5465efd87c0
dabouledida <dimitrisdim8900@gmail.com>
2022-02-27 22:29:13 +0200

    Added isAssignment. Started isrelOperator.

--- lex.py
+++ lex.py
@@ -25,6 +25,37 @@ def isKeyword(file):
             else:
                 recognized_string = recognized_string + character
 
+def isAssignment(file):
+    recognized_string = ""
+    first_character = file.read(1)
+    second_character = file.read(1)
+    recognized_string = first_character + second_character
+    if recognized_string == ":=":
+        return recognized_string, "assignment"
+
+def isrelOperator(file):
+    recognized_string = ""
+    while True:
+        character = file.read(1)
+        if character == ">":
+            second_char = file.read(1)
+            if second_char == "=":
+                recognized_string = character + second_char
+                return recognized_string, "relOperator"
+            else:
+                recognized_string = second_char
+                return character, "relOperator"
+        elif character == "<":
+            second_char = file.read(1)
+            if second_char == "=" or second_char == ">":
+                recognized_string = character + second_char
+                return recognized_string, "relOperator"
+            else:
+                recognized_string = second_char
+                return character, "relOperator"
+
+#TODO: Want to pass the seconf char to the next call so as the next call does not lose the part we have readen.
+
 def isSimple(file):
     group_symbol_list = ["{", "}", "(", ")", "[", "]"]
     delimeter_list = [",", ";", "."]
@@ -55,21 +86,21 @@ def lex(file):
     family = ""
     line_counter = 0
 
-    clearBlankChar(file)
-
-    recognized_string, family = isNumber(file)
+    #recognized_string, family = isNumber(file)
     #recognized_string, family = isSimple(file)
     #recognized_string, family = isKeyword(file)
+    #recognized_string, family = isAssignment(file)
+    recognized_string, family = isrelOperator(file)
 
     print(recognized_string + " family: " + family + " line: ", line_counter)
     return Token(recognized_string, family, line_counter)
 
 def main():
-    #if(len(sys.argv) < 3):
-            #print(sys.argv[1]) 
+    if(len(sys.argv) < 3):
+            print(sys.argv[1]) 
 
-    with open(sys.argv[1], "r") as file:
-        lex(file)
+    file = open(sys.argv[1], "r")
+    lex(file)
 
 if __name__ == "__main__":
     main()
